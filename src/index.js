7e68cc300995fcb0a7b6238982c7be53ab75597d
Shaunak Basu <55021457+basushaunak@users.noreply.github.com>
2025-06-19 07:51:11 +0530

    WIP - no GUI

--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
-import {runTodoApp} from './scripts/runtodoapp.js';
-document.querySelector("body").innerHTML = `<h1>Open the Console....`;
+import {runTodoApp} from "./scripts/runtodoapp.js";
+// document.querySelector("body").innerHTML = `<h1>Open the Console....</h1>`;
+runTodoApp();
